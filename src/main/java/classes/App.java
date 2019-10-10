98ac5025603c684e38aa3f3648b6f26130cda063
surya <gurram.suryateja@gmail.com>
2019-10-10 17:06:14 +0530

    first commit

---
+++ src/main/java/classes/App.java
@@ -0,0 +1,13 @@
+package classes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void test ()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/testcases/sample.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+import classes.App;
+
+public class sample {
+	
+	@Test
+	public static void testone () {
+		App.test();
+	}
+
+}
