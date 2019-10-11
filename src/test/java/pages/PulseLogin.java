ff10ac955e463ca2f871c9e2ddc2cf678e245a94
surya <gurram.suryateja@gmail.com>
2019-10-11 15:56:26 +0530

    Second commit

---
+++ src/test/java/pages/PulseLogin.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class PulseLogin {
+
+	public static WebDriver driver;
+
+	public static By username = By.id("P101_USERNAME");
+	public static By password = By.id("P101_PASSWORD");
+	public static By loginbutton = By.xpath("//*[@id=\"B2188001518354150704\"]/span[2]");
+
+	public static void pulselogin() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://183.82.125.132:8460/ords/f?p=333:LOGIN_DESKTOP::::::");
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		driver.findElement(username).sendKeys("stgurram");
+		driver.findElement(password).sendKeys("pulse456");
+		driver.findElement(loginbutton).click();
+		Thread.sleep(10000);
+		driver.close();
+			
+		}
+		
+
+	}
+
---
+++ src/test/java/testcases/TC_PulseLogin.java
@@ -0,0 +1,27 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+import pages.PulseLogin;
+
+public class TC_PulseLogin {
+	
+	@Test
+	public static void TC_login() {
+		
+		try {
+			PulseLogin.pulselogin();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	
+	
+	
+	
+	  
+	 }
+	
+}
